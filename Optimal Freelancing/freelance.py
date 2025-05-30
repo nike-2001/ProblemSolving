babcf803752870347a24219180af083c79df0bb2
Venkata Nikhil Chakravarthy Korrapati <nikekorrapati@gmail.com>
2025-05-30 11:46:39 -0400

    Update freelance.py

--- Optimal Freelancing/freelance.py
+++ Optimal Freelancing/freelance.py
@@ -1,1 +1,15 @@
+def optimalFreelancing(jobs):
+    max_days = 7
+    max_profit = 0
+    jobs.sort(key = lambda k: k["payment"], reverse = True)
+    days = [False] * (max_days + 1)
 
+    for job in jobs:
+        deadline = min(job["deadline"], max_days)
+        for i in range(deadline, 0, -1):
+            if days[i] == False:
+                days[i] = True
+                max_profit += job["payment"]
+                break
+
+    return max_profit
