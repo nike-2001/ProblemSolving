bb8be000a3c8f3c71f01a75514c5a7fa6ec189e2
Venkata Nikhil Chakravarthy Korrapati <nikekorrapati@gmail.com>
2025-06-01 23:17:47 -0400

    Update duplicate-LL.py

--- Remove Duplicates From LL/duplicate-LL.py
+++ Remove Duplicates From LL/duplicate-LL.py
@@ -1,1 +1,17 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+    current = linkedList
+    while current is not None and current.next is not None:
+        if current.value == current.next.value:
+            current.next = current.next.next
+        else:
+            current = current.next
+
+    return linkedList
 
